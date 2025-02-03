c65f0e55c0aac558ba15e394787e22847e279166
kai-xlr <xhauma@protonmail.com>
2025-02-02 19:13:00 -0500

    2. Set the FPS and created a delta counter for efficiency

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 from constants import *
 
+
+
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    SCREEN_WIDTH
-    SCREEN_HEIGHT
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black", rect=None, special_flags=0)
+            
+        screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
