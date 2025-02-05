773cec6513c711c0117537f1b9ffb6aeffbd3191
kai-xlr <xhauma@protonmail.com>
2025-02-04 21:50:35 -0500

    12. Added splitting functionality

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
             for shot in shots:
                 if asteroid.collide(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
