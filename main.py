f5890158627f8e9017af6c92114ce0c3860bb3d9
kai-xlr <xhauma@protonmail.com>
2025-02-04 21:11:00 -0500

    9. Projectile creation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,12 +15,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable) # type: ignore
+    Shot.containers = (shots, updatable, drawable) # type: ignore
+    AsteroidField.containers = updatable # type: ignore
     asteroid_field = AsteroidField()
 
-    Player.containers = (updatable, drawable)
+    Player.containers = (updatable, drawable) # type: ignore
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
